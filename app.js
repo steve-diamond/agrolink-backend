6f1742d75c66daf36868d6761efc54c37a1d9ffb
stephen <ebonytblues@gmail.com>
2026-03-29 21:45:29 +0100

    Rename rateLimit to rateLimiter in app.js for consistency

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const rateLimit = require('../middleware/rateLimiter');
+const rateLimiter = require('../middleware/rateLimiter');
 
 const apiRoutes = require('../routes');
 const adminRoutes = require('../routes/adminRoutes');
@@ -21,7 +21,7 @@ const configuredOrigins = process.env.CORS_ORIGIN
 const corsOrigin = configuredOrigins.includes('*') ? true : configuredOrigins;
 app.use(cors({ origin: corsOrigin }));
 app.use(helmet());
-app.use(rateLimit);
+app.use(rateLimiter);
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
