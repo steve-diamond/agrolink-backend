b2049d27383778e8f300c123e4fa67dca61e0091
stephen <ebonytblues@gmail.com>
2026-03-30 19:06:57 +0100

    fix: use absolute path for validation import in order.routes.js for Render compatibility

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -7,7 +7,8 @@ const {
   updateOrderStatus,
 } = require('../controllers/order.controller');
 const { protect, authorize } = require('../middleware/auth.middleware');
-const { body, param, validate } = require('../../middleware/validation');
+const path = require('path');
+const { body, param, validate } = require(path.join(__dirname, '../middleware/validation'));
 
 const router = express.Router();
 
