56cebc01a2b2c2b23fd77681a966cac389819248
stephen <ebonytblues@gmail.com>
2026-03-30 17:16:31 +0100

    fix: use absolute path for apiError import in auth.middleware.js for Render compatibility

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const path = require('path');
 const User = require(path.join(__dirname, '../models/User'));
 const asyncHandler = require(path.join(__dirname, '../utils/asyncHandler'));
-const ApiError = require('../../utils/apiError');
+const ApiError = require(path.join(__dirname, '../utils/apiError'));
 
 const protect = asyncHandler(async (req, res, next) => {
   const authHeader = req.headers.authorization || '';
