9477fb82da93e4e850d8c2ed0988335acc9a4648
stephen <ebonytblues@gmail.com>
2026-03-30 20:30:06 +0100

    fix: remove duplicate mongoose imports and schema definitions in Wallet and WalletTransaction models

--- models/WalletTransaction.js
+++ models/WalletTransaction.js
@@ -1,37 +1,5 @@
 const mongoose = require("mongoose");
 
-const walletTransactionSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    type: {
-      type: String,
-      enum: ["credit", "debit"],
-      required: true,
-    },
-    amount: {
-      type: Number,
-      required: true,
-    },
-    reference: {
-      type: String,
-      default: null,
-    },
-    description: {
-      type: String,
-      default: "",
-    },
-    createdAt: { type: Date, default: Date.now },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("WalletTransaction", walletTransactionSchema);
-const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const WalletTransactionSchema = new Schema({
