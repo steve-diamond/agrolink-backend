c5d807cd9a81a7955806cab925ba4ce5fcffec12
stephen <ebonytblues@gmail.com>
2026-03-30 23:08:30 +0100

    fix: replace circular require in adminRoutes.js with real router

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,1 +1,21 @@
-module.exports = require("../../routes/adminRoutes");
+const express = require('express');
+const { getAllUsers, getAllProducts, approveProduct, deleteUser, getAllOrders } = require('../controllers/adminController');
+
+const router = express.Router();
+
+// Get all users
+router.get('/users', getAllUsers);
+
+// Get all products
+router.get('/products', getAllProducts);
+
+// Approve a product
+router.patch('/products/:productId/approve', approveProduct);
+
+// Delete a user
+router.delete('/users/:userId', deleteUser);
+
+// Get all orders
+router.get('/orders', getAllOrders);
+
+module.exports = router;
